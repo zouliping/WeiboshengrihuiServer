39547f96a0007a424aa66bcf12bbede01e3e97fb
zouliping <zouliping007@163.com>
2014-04-07 21:06:29 +0800

    登陆成功后，给用户返回其唯一标识

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,14 @@
 package controllers;
 
 import models.Users;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import utils.JsonUtil;
+import utils.SHA1;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Application extends Controller {
 
@@ -30,7 +33,9 @@ public class Application extends Controller {
 			return badRequest(JsonUtil.getFalseJson());
 		}
 		if (user.pwd.equals(pwd)) {
-			return ok(JsonUtil.getTrueJson());
+			ObjectNode on = Json.newObject();
+			on.put("result", SHA1.getSHA1String(uid));
+			return ok(on);
 		} else {
 			return badRequest(JsonUtil.getFalseJson());
 		}
